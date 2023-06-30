84c69124ec400172b76f3e0659631aa2139f6596
maddalenabin <maddalenabin@gmail.com>
2023-06-30 20:33:25 +0200

    updated analysis notebooks

--- 01-notebooks/functions/h5_tools.py
+++ 01-notebooks/functions/h5_tools.py
@@ -1,6 +1,8 @@
 import h5py
 import numpy as np
-
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
 
 def load_data(file, key):
     """Load dataset of a h5 file and return the entry as np.array
@@ -9,15 +11,15 @@ def load_data(file, key):
     with h5py.File(file, 'r') as f:
         data = f[key]
         data = np.asarray(data)
-    
+            
     return data
 
 
 def visit_func(name, node):
-    '''Return all groups and datasets name and shapes of h5 file called name
+    """Return all groups and datasets name and shapes of h5 file called name
     Use it as in the following:
     with h5py.File(filename, 'a') as f: f.visititems(visit_func)
-    '''
+    """
     if isinstance(node, h5py.Group):
         print("group", node.name)
     elif isinstance(node, h5py.Dataset):
@@ -28,15 +30,39 @@ def visit_func(name, node):
     else:
         print(node.name, 'is an unknown type')
 
-# def elog_entries():
-#     entries = ['elog/Absorbers 100um/', 'elog/Absorbers 80um/', 'elog/Exposure time (s)/', 'elog/LN flow meter/', 'elog/Mesh (spots x lines)/', 'elog/No of spots/', 'elog/Sample/', 'elog/Sample no. (label)/', 'elog/Short comment/', 'elog/Temperature, C/', 'elog/Temperature, K/', 'elog/Total exposure time (s)/', 'elog/comment/', 'elog/measurement folder/', 'elog/number of frames/', 'elog/position y, mm/', 'elog/position z, mm/', 'elog/reps per spot/', 'elog/scan number/', 'elog/transmission (%)/']    return entries
-#     return entries
 
-# def saxs_entries():
-#     entries = ['saxs/I/', 'saxs/I_reps/', 'saxs/q/', 'saxs/scans/']
-#     return entries
+def elog_selected_entries(filename, entries=["measurement folder","Temperature, K","transmission (%)","Absorbers 80um","Absorbers 100um","Short comment","comment","Exposure time (s)","number of frames",]):
+    """Return the elog entries given, taken from the google drive sheet of the beamtime
+    """
+    with h5py.File(filename, 'r') as f: 
+        for entry in entries:
+            print("{0:20} {1}".format(entry,  f[f'elog/{entry}/'].asstr()[()]) )
+            
+def elog_selected_entries_dict(filename, entries=["measurement folder","Temperature, K","transmission (%)","Absorbers 80um","Absorbers 100um","Short comment","comment","Exposure time (s)","number of frames",]):
+    """Return a dictionary with the elog entries given, taken from the google drive sheet of the beamtime
+    """
+    d = {}
+    with h5py.File(filename, 'r') as f: 
+        for entry in entries:
+            d[entry] = f[f'elog/{entry}/'].asstr()[()]
+    return d
 
-# def xpcs_entries():
-#     g2s = ['xpcs/g2s/delay/', 'xpcs/g2s/g2s/', 'xpcs/g2s/q/']
-#     ttcs = ['xpcs/ttcs/q/', 'xpcs/ttcs/times/', 'xpcs/ttcs/ttc_avg_int/', 'xpcs/ttcs/ttc_rep_qs_avg/']
-#     return g2s + ttcs
+def plot_ttc(filename, rep=0, vs=(0,0.15)):
+    """Plots the twotime correlation function for different momentum transfers"""
+    ttc_qv = load_data(filename, '/xpcs/ttcs/q')
+    time = load_data(filename, '/xpcs/ttcs/times')
+    
+    ttcs = np.empty(shape=(len(ttc_qv), len(time), len(time)))
+    with h5py.File(filename, 'r') as f:
+        ttcs = np.asarray(f['/xpcs/ttcs/ttc_rep_qs_avg'])[rep,:,:,:]
+        
+    f, axs = plt.subplots(2,5,figsize=(10,4), tight_layout=True)
+    for i in range(len(ttc_qv)):
+        ax = axs.ravel()[i]
+        norm = np.mean(ttcs[i,:300,-300:])
+        ax.imshow(ttcs[i,:,:]-norm, origin='lower', cmap='turbo', vmin=vs[0], vmax=vs[1], extent=(time[0],time[-1])*2 )
+        ax.set_xlabel('$t_1 (s)$')
+        ax.set_title(f'{ttc_qv[i]:.2f} 1/nm', fontsize=8)
+        ax.set_ylabel('$t_2 (s)$')
+        divider = make_axes_locatable(ax)
+        cb1 = mpl.colorbar.ColorbarBase(divider.append_axes('right', size='5%', pad=0.05), cmap=mpl.cm.turbo,  orientation='vertical', norm=mpl.colors.Normalize(vmin=0,vmax=.15));
