93166cc7e139299e5e05ce1ca6eaa40f3cda9fcf
maddalenabin <maddalenabin@gmail.com>
2024-12-13 14:49:26 +0100

    new fig3 notebooks

---
+++ 01-notebooks/functions/metadata.py
@@ -0,0 +1,17 @@
+import re
+
+def get_scan_number(s):
+    s = str(s)
+    s = re.search('(?<=scan)\d{4}', s)
+    if s is not None:
+        return int(s.group(0))
+    else:
+        return -1
+    
+    
+def get_rep(x, reps_per_spot=1):
+    scan = get_scan_number(x)
+    rep = scan % reps_per_spot
+    if rep == 0:
+        rep = 4
+    return rep
